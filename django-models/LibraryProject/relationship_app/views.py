8ac7474ba111e807a91387b9960273b8e0d64130
Selaelo2 <selaelolanga10@gmail.com>
2025-03-04 10:01:51 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Book, Library  # Correct import for Library model
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Function-based view to list all books
-def list_books(request):
-    books = Book.objects.all()  # Get all books from the database
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+# Register View: Handle user registration
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Automatically log in the user after registration
+            username = form.cleaned_data.get('username')
+            user = form.save()
+            login(request, user)
+            return redirect('login')  # Redirect to login page after registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
 
-# Class-based view to display details of a specific library
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+# Login View: Use Django's built-in login view
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+# Logout View: Use Django's built-in logout view
+class CustomLogoutView(LogoutView):
+    next_page = '/'  # Redirect to homepage after logout
