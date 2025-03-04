e5b0074d40bc597c596876998234636249932bb4
Selaelo2 <selaelolanga10@gmail.com>
2025-03-04 10:01:55 +0200

    Update urls.py

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import list_books, LibraryDetailView
+from .views import register, CustomLoginView, CustomLogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('books/', list_books, name='list_books'),  # Route for function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Route for class-based view
+     path('register/', register, name='register'),  # Registration view
+    path('login/', CustomLoginView.as_view(), name='login'),  # Login view
+    path('logout/', CustomLogoutView.as_view(), name='logout'),  # Logout view
 ]
 
