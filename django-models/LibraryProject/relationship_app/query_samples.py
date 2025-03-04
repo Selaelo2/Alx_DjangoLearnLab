03f7c610eec5bb9ce95f12d00a6e26ee4510ce69
Selaelo2 <selaelolanga10@gmail.com>
2025-03-04 09:38:15 +0200

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,7 +4,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 def get_books_by_author():
     author_name = 'J.K. Rowling'
     author = Author.objects.get(name=author_name)
-    books = author.books.all()
+    books = Book.objects.filter(author=author)  # Using filter to get books by the specific author
     for book in books:
         print(f'Book: {book.title} by {author.name}')
 
@@ -12,7 +12,7 @@ def get_books_by_author():
 def get_books_in_library():
     library_name = 'Central Library'
     library = Library.objects.get(name=library_name)
-    books = library.books.all()
+    books = library.books.all()  # This works since it's a ManyToMany relationship
     for book in books:
         print(f'Book: {book.title} in {library.name}')
 
@@ -20,7 +20,7 @@ def get_books_in_library():
 def get_librarian_for_library():
     library_name = 'Central Library'
     library = Library.objects.get(name=library_name)
-    librarian = library.librarian
+    librarian = library.librarian  # Since it's a OneToOneField relationship
     print(f'Librarian for {library.name}: {librarian.name}')
 
 
