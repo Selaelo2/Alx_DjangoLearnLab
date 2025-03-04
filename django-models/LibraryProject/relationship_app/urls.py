f6f8091e74ea9fa238ed2567baccfa87a102381a
Selaelo2 <selaelolanga10@gmail.com>
2025-03-04 10:16:08 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include  # include is required to include the app URLs
-from .views import list_books, LibraryDetailView, register, user_login, user_logout
+from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('relationship/', include('relationship_app.urls')),  # Include the relationship app URLs
       path('books/', list_books, name='list_books'),  # Function-based view for listing books
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view for library details
-    path('register/', register, name='register'),  # Registration view
-    path('login/', user_login, name='login'),  # Login view
-    path('logout/', user_logout, name='logout'),  # Logout view
+    path('register/', views.register, name='register'),  # Registration view
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # Login view
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),  # Logout view
 ]
