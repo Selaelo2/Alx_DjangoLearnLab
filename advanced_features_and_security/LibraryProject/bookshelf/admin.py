3b7f4175e4b4c953e8d455e921b45bc360593499
Selaelo2 <selaelolanga10@gmail.com>
2025-03-10 09:18:58 +0200

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -11,6 +11,7 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('publication_year',)
 
     # bookshelf/admin.py
+# bookshelf/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
@@ -23,8 +24,15 @@ class CustomUserAdmin(UserAdmin):
     ordering = ['username']
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'profile_photo')
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'date_of_birth', 'profile_photo', 'is_staff', 'is_active'),
+        }),
+    )
 
-
-
-
+admin.site.register(CustomUser, CustomUserAdmin)
