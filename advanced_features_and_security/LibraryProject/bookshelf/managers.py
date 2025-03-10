6669946ae4fb761a26dfe9f63a0e34687c79c9d8
Selaelo2 <selaelolanga10@gmail.com>
2025-03-10 09:07:33 +0200

    Create managers.py

---
+++ advanced_features_and_security/LibraryProject/bookshelf/managers.py
@@ -0,0 +1,25 @@
+# bookshelf/managers.py
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password, date_of_birth=None, profile_photo=None, **extra_fields):
+        """
+        Create and return a regular user with an email, username, password, 
+        date_of_birth, and profile_photo.
+        """
+        if not username:
+            raise ValueError("The Username must be set")
+        user = self.model(username=username, date_of_birth=date_of_birth, profile_photo=profile_photo, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password, date_of_birth=None, profile_photo=None, **extra_fields):
+        """
+        Create and return a superuser with a username, password, date_of_birth,
+        and profile_photo.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(username, password, date_of_birth, profile_photo, **extra_fields)
