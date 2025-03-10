c5d722c42d7f0f49d89c16a233c32fd4fc9186ce
Selaelo2 <selaelolanga10@gmail.com>
2025-03-10 09:06:16 +0200

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -11,14 +11,4 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('publication_year',)
 
 
-# bookshelf/models.py
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-
-    def __str__(self):
-        return self.username
 
