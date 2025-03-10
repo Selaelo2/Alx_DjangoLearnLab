aa25fcce714212fc586a5bec917a0b91d38f5759
Selaelo2 <selaelolanga10@gmail.com>
2025-03-10 09:19:14 +0200

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -8,19 +8,21 @@ class Book(models.Model):
     def __str__(self):
         return f"{self.title} by {self.author}"
     
+
+    
     # bookshelf/models.py
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from .managers import CustomUserManager
+from .managers import CustomUserManager  # Import the custom manager
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
-    # Use the custom manager for user creation
-    objects = CustomUserManager()
+    objects = CustomUserManager()  # Use the custom manager
 
     def __str__(self):
         return self.username
+
 
 
