2fca0786b389efb5269e17ae567eac9ca81550ce
Selaelo2 <selaelolanga10@gmail.com>
2025-03-15 14:05:50 +0200

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -9,12 +9,12 @@ urlpatterns = [
     # Create a new book
     path('books/create/', BookCreate.as_view(), name='book-create'),
 
-    # Retrieve a specific book
+    # Retrieve a specific book by ID
     path('books/<int:pk>/', BookDetail.as_view(), name='book-detail'),
 
-    # Update a specific book (with a unique ID in the URL)
+    # Update a specific book (with the book's ID)
     path('books/<int:pk>/update/', BookUpdate.as_view(), name='book-update'),
 
-    # Delete a specific book (with a unique ID in the URL)
+    # Delete a specific book (with the book's ID)
     path('books/<int:pk>/delete/', BookDelete.as_view(), name='book-delete'),
 ]
