e74bc0f9521677eddbe47d9d7e83a3b029148d83
Selaelo2 <selaelolanga10@gmail.com>
2025-03-15 13:22:03 +0200

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,7 +4,9 @@
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()  # Query to fetch all books
     serializer_class = BookSerializer  # Serializer to convert book data to JSON format
+    permission_classes = [IsAuthenticated] 
