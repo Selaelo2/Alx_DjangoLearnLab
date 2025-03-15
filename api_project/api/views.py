91443c5146fe4a4dbc5945858527f47f9f206c23
Selaelo2 <selaelolanga10@gmail.com>
2025-03-15 13:26:15 +0200

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,7 +4,7 @@
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()  # Query to fetch all books
