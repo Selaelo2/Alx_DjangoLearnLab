e5c9b77f8cfa98e62f26013229f37a6b2d8c1889
Selaelo2 <selaelolanga10@gmail.com>
2025-03-16 18:58:57 +0200

    changes

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+    
+    # Author model represents an author with a name.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# Book model represents a book with a title, publication year, and a foreign key to Author.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
