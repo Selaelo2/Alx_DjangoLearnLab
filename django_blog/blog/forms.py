64f42f30b12e130a9b09912d7039add3b5237de8
Selaelo2 <selaelolanga10@gmail.com>
2025-03-23 15:27:48 +0200

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -26,7 +26,7 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ["title", "content", "tags"]
         widgets = {
-            "tags": TagWidget(attrs={"placeholder": "Add tags separated by commas"}),
+            "tags": TagWidget(attrs={"placeholder": "Add tags separated by commas"}),TagWidget(),
         }
 
 class CommentForm(forms.ModelForm):
