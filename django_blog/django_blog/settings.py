a2d1aec144a9e05c1dc39ee21f2ff81bbe89189a
Selaelo2 <selaelolanga10@gmail.com>
2025-03-23 14:26:22 +0200

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'your_database_user',
+        'PASSWORD': 'your_database_password',
+        'HOST': 'localhost',  # or your database host
+        'PORT': '3306',       # default MySQL port
     }
 }
 
