78e4db96f85699a960e841f43d07d37827eb4ca7
Selaelo2 <selaelolanga10@gmail.com>
2025-03-23 14:48:22 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path("profile/update/", views.update_profile, name="update_profile"),
     path("", PostListView.as_view(), name="post_list"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
-    path("posts/new/", PostCreateView.as_view(), name="post_create"),
-    path("post/<int:pk>/edit/", PostUpdateView.as_view(), name="post_update"),
-    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post/new/"),
+    path("posts/new/", PostCreateView.as_view(), name="post/new/"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post_update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
 ]
